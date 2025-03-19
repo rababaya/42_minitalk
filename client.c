39ec5c59a061aad3b20fcb0e2564aeee001ee823
Rafik Babayan <rababaya@c2r11s6.42yerevan.am>
2025-03-19 17:55:39 +0400

    xelqin glux

--- client.c
+++ client.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rababaya <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/13 15:01:15 by rababaya          #+#    #+#             */
-/*   Updated: 2025/03/13 15:40:03 by rababaya         ###   ########.fr       */
-/*                                                                            */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 client.c											:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: rababaya <marvin@42.fr>					+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2025/03/13 15:01:15 by rababaya		   #+#	  #+#			  */
+/*	 Updated: 2025/03/18 14:57:28 by rababaya		  ###	########.fr		  */
+/*																			  */
 /* ************************************************************************** */
 
 #include <stdio.h>
@@ -15,8 +15,43 @@
 #include <unistd.h>
 #include <signal.h>
 
+void send_bit(char sending, pid_t server_pid)
+{
+	unsigned char	a;
+	int		i;
+
+	i = -1;
+	a = 128;
+	while (++i < 8)
+	{
+		printf("step:%d\n", i);
+		if (sending & (a >> i))
+		{
+			printf("1\n");
+			kill(server_pid, SIGUSR1);
+		}
+		else
+		{
+			printf("0\n");
+			kill(server_pid, SIGUSR2);
+		}
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
-		kill(atoi(argv[1]), SIGUSR1);
+	pid_t			server_pid;
+	unsigned char	*massage;
+	if (argc == 3)
+	{
+		server_pid = atoi(argv[1]);
+		massage = argv[2];
+
+		while (*massage)
+		{
+			send_bit(*massage, server_pid);
+			massage++;
+		}
+		send_bit('\0', server_pid);
+	}
 }
--- server.c
+++ server.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rababaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/13 13:47:09 by rababaya          #+#    #+#             */
-/*   Updated: 2025/03/13 15:33:02 by rababaya         ###   ########.fr       */
+/*   Created: 2025/03/19 17:04:11 by rababaya          #+#    #+#             */
+/*   Updated: 2025/03/19 17:29:50 by rababaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,36 @@
 #include <unistd.h>
 #include <signal.h>
 
-void	sigusr1_handler(int sig)
+void	sig_handler(int sig)
 {
-	printf("hi\n");
+	static unsigned char	a = 0;
+	static int				bit = 0;
+
+	if (sig == SIGUSR1)
+		a |= (128 >> bit);
+	else
+		a |= 0;
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &a, 1);
+		bit = 0;
+		a = 0;
+	}
 }
 
 int	main(void)
 {
-	struct sigaction	sigact;
+	struct sigaction	sig;
 
 	printf("%d\n", getpid());
-	sigact.sa_handler = &sigusr1_handler;
-	sigact.sa_flags = SA_RESTART;
-	sigaction(SIGUSR1, &sigact, NULL);
+	sig.sa_handler = &sig_handler;
+	sig.sa_flags = SA_RESTART;
+	sigemptyset(&sig.sa_mask);
+	sigaddset(&sig.sa_mask, SIGUSR1);
+	sigaddset(&sig.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
 	while (1)
 	{
 		usleep(1);
